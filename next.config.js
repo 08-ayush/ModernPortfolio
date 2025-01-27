27f8c939c4d154876dc337d64a13ac5fbe84c3de
08-ayush <aayushchauhan008@gmail.com>
2025-01-27 22:33:49 +0530

    "removing error"

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'export',  // Add this for static export
+  output: 'export',  // Keep this for static export
+  images: {
+    unoptimized: true,  // Disable image optimization for static export
+  },
 };
 
 module.exports = nextConfig;
